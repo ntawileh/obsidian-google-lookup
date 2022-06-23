8f8a88eef9670c0abda894c2149e75da44de111d
Nadim Tawileh <nadim@tawileh.com>
2022-06-23 09:47:14 -0700

    handle empty credentials when trying to use a command

--- src/settings/google-credentials.ts
+++ src/settings/google-credentials.ts
@@ -16,5 +16,5 @@ export const hasGoogleCredentials = (plugin: GoogleLookupPlugin): boolean => {
 	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 	const { client_id, client_redirect_uri_port, client_secret } = plugin.settings!;
 
-	return client_id.length > 0 && client_secret.length > 0 && client_redirect_uri_port.length > 0;
+	return client_id?.length > 0 && client_secret?.length > 0 && client_redirect_uri_port?.length > 0;
 };
