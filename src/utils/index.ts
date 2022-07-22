fda8f96444ee0d7e7a7c8b332b75838ac36c494c
Nadim Tawileh <nadim@tawileh.com>
2022-07-22 11:18:01 -0700

    fix birthday format to include leading 0 in mo/day as needed

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,8 @@
+import { people_v1 } from '@googleapis/people';
+
 export * from './files';
 export * from './template';
+
+export const formatBirthday = ({ year, month, day }: people_v1.Schema$Date): string => {
+	return [year, `0${month}`.slice(-2), `0${day}`.slice(-2)].join('-');
+};
